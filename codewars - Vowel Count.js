f171ab408b13435fa1bb0cb97511573fe5766af1
Davi Moraes Ladeira <davimoraesladeira@hotmail.com>
2022-03-27 23:19:16 -0300

    vowel

---
+++ codewars - Vowel Count.js
@@ -0,0 +1,16 @@
+
+function vowelCount(str)
+{
+    for(i=0;i<str.length;i++)
+    {
+        if(str[i]=='a' | str[i]=='e' | str[i]=='i' | str[i]== 'o' | str[i]== 'u')
+        {
+            console.log(str[i])
+        }
+        
+    }
+}
+
+let str = "davi"
+
+vowelCount(str)
